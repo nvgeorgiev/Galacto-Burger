5d9e74266fb8c86bc237ba35b41ea768a3240ffc
Nikolay Georgiev <nikolaay.georgiev@gmail.com>
2020-11-28 19:12:07 +0200

    Implemented static controls for adding ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import './Burger.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Ingredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Add your favourite ingredients...</p>;
+  }
+
   return (
     <div className="Burger">
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {transformedIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
+
   render() {
     return (
       <Auxiliary>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Auxiliary>
     );
   }
