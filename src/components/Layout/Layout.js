4ac7624c9690a875fd043b3d8b9c5d366ae3af48
Nikolay Georgiev <nikolaay.georgiev@gmail.com>
2020-12-01 09:17:23 +0200

    Added a static toolbar and a sidedrawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Layout.css';
 
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </Auxiliary>
 );
